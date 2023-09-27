385fdfdbbe646318ddad8fa74242df1b2656ce82
ZariaNYU <144282037+ZariaNYU@users.noreply.github.com>
2023-09-27 08:51:26 -0400

    HW03A Iteration 2
    In this iteration, I nested the for loops.

--- sketch.js
+++ sketch.js
@@ -5,6 +5,13 @@ let xSpacing1 = 50;
 let yOffset1 = 0;
 let ySpacing1 = 50;
 
+let cDiam2 = 5;
+let xOffset2 = 50;
+let xSpacing2 = 50;
+
+let yOffset2 = 50;
+let ySpacing2 = 50;
+
 function setup() {
   createCanvas(400, 400);
 }
@@ -14,23 +21,13 @@ function draw() {
 
   for (let xPos1 = xOffset1; xPos1 <= width - xOffset1; xPos1 += xSpacing1) {
       for (let yPos1 = yOffset1; yPos1 <= height - yOffset1; yPos1 += ySpacing1) {
-      fill(0, 0, 0); 
-      circle(xPos1, yPos1, cDiam1);
-    }
-  }
-
-
-let cDiam2 = 5;
-let xOffset2 = 50;
-let xSpacing2 = 50;
-
-let yOffset2 = 50;
-let ySpacing2 = 50;
-
-  for (let xPos2 = xOffset2; xPos2 <= width - xOffset2; xPos2 += xSpacing2) {
-    for (let yPos2 = yOffset2; yPos2 <= height - yOffset2; yPos2 += ySpacing2) {
-    fill(0, 0, 0); 
-    circle(xPos2, yPos2, cDiam2);
-    }
+        for (let xPos2 = xOffset2; xPos2 <= width - xOffset2; xPos2 += xSpacing2) {
+          for (let yPos2 = yOffset2; yPos2 <= height - yOffset2; yPos2 += ySpacing2) {
+          fill(0, 0, 0); 
+          circle(xPos1, yPos1, cDiam1);
+          circle(xPos2, yPos2, cDiam2);
+        }
+       }
+      }
   }
 }
