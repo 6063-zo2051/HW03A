513498a83ac69532d5067ae61ca843129a8bc0df
ZariaNYU <144282037+ZariaNYU@users.noreply.github.com>
2023-09-27 11:15:36 -0400

    HW03A Iteration 4
    Completed Image

--- sketch.js
+++ sketch.js
@@ -5,12 +5,19 @@ let xSpacing1 = 50;
 let yOffset1 = 0;
 let ySpacing1 = 50;
 
-let cDiam2 = 5;
-let xOffset2 = 50;
-let xSpacing2 = 50;
+let cDiam2 = 10;
+let xOffset2 = 0;
+let xSpacing2 = 25;
 
-let yOffset2 = 50;
-let ySpacing2 = 50;
+let yOffset2 = 0;
+let ySpacing2 = 25;
+
+let cDiam3 = 15;
+let xOffset3 = 25;
+let xSpacing3 = 50;
+
+let yOffset3 = 25;
+let ySpacing3 = 50;
 
 function setup() {
   createCanvas(400, 400);
@@ -21,13 +28,23 @@ function draw() {
 
   for (let xPos1 = xOffset1; xPos1 <= width - xOffset1; xPos1 += xSpacing1) {
       for (let yPos1 = yOffset1; yPos1 <= height - yOffset1; yPos1 += ySpacing1) {
-        for (let xPos2 = xOffset2; xPos2 <= width - xOffset2; xPos2 += xSpacing2) {
-          for (let yPos2 = yOffset2; yPos2 <= height - yOffset2; yPos2 += ySpacing2) {
-          fill(0, 0, 0); 
-          circle(xPos1, yPos1, cDiam1);
-          circle(xPos2, yPos2, cDiam2);
+        fill(0, 0, 0);   
+        circle(xPos1, yPos1, cDiam1);
         }
        }
+
+  for (let xPos2 = xOffset2; xPos2 <= width - xOffset2; xPos2 += xSpacing2) {
+     for (let yPos2 = yOffset2; yPos2 <= height - yOffset2; yPos2 += ySpacing2) {
+        fill(0, 0, 0); 
+        circle(xPos2, yPos2, cDiam2);
+     }
       }
-  }
-}
+
+  for (let xPos3 = xOffset3; xPos3 <= width - xOffset3; xPos3 += xSpacing3) {
+      for (let yPos3 = yOffset3; yPos3 <= height - yOffset3; yPos3 += ySpacing3) {
+          fill(0, 0, 0);   
+          circle(xPos3, yPos3, cDiam3);
+          }
+         }
+
+    }
